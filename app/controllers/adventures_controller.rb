41c87b0d08768e78ce299abf910ca64168115dd1
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2017-03-07 15:23:17 -0800

    add logo to home page, render the correct photos using instagram API

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,11 +1,10 @@
 require 'instagram'
 class AdventuresController < ApplicationController
 
-def index
-
-   client = Instagram.client(:access_token => ENV['config.access_token'] )
-   @user = client.user
-   @recent_media_items = client.user_recent_media
-end
+  def index
+     client = Instagram.client(:access_token => ENV['config.access_token'] )
+     @user = client.user
+     @recent_media_items = client.user_recent_media
+  end
 
 end
