a147e37bc2b27bf5eeaf39833ae502b5dbbebe95
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2017-03-20 13:34:41 -0600

    commit changes to seed file to deploy to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-product1 = Product.create([{ title: 'Cranberry Citrus', image: File.new('https://images.pexels.com/photos/3672/food-fruits-cereals-breakfast.jpg?w=940&h=650&auto=compress&cs=tinysrgb'), flavor: 'Cranberry Citrus', size: '12 oz', price: 5, description: 'A super yummy combination of Cranberry and Citrus.'}])
+product1 = Product.create([{ title: 'Cranberry Citrus', image: File.new('app/assets/images/oatmeal2.jpeg'), flavor: 'Cranberry Citrus', size: '12 oz', price: 5, description: 'A super yummy combination of Cranberry and Citrus.'}])
 
-product2 = Product.create([{ title: 'Apple Cinnamon Raisin', image: File.new('https://images.pexels.com/photos/3672/food-fruits-cereals-breakfast.jpg?w=940&h=650&auto=compress&cs=tinysrgb'), flavor: 'Cranberry Citrus', size: '12 oz', price: 5, description: 'A super yummy combination of Cranberry and Citrus.'}])
+product2 = Product.create([{ title: 'Apple Cinnamon Raisin', image: File.new('app/assets/images/oatmeal2.jpeg'), flavor: 'Cranberry Citrus', size: '12 oz', price: 5, description: 'A super yummy combination of Cranberry and Citrus.'}])
 
-product3 = Product.create([{ title: 'Blueberry Coconut', image: File.new('https://images.pexels.com/photos/3672/food-fruits-cereals-breakfast.jpg?w=940&h=650&auto=compress&cs=tinysrgb'), flavor: 'Cranberry Citrus', size: '12 oz', price: 5, description: 'A super yummy combination of Cranberry and Citrus.'}])
+product3 = Product.create([{ title: 'Blueberry Coconut', image: File.new('app/assets/images/oatmeal2.jpeg'), flavor: 'Cranberry Citrus', size: '12 oz', price: 5, description: 'A super yummy combination of Cranberry and Citrus.'}])
